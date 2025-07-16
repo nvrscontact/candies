1cd6b2bbbcee13bd5b61ef5c2bcf64be3539695d
nvrsunk <nvrsdark@gmail.com>
2025-07-16 21:50:31 +0200

    g

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -14,9 +14,9 @@ function HomePage() {
   return (
     <>
     
-    <section className='mt-4 flex flex-col items-center '>
+    <section className=' '>
        
-       <div className='mb-12 w-full'>
+       <div className='mb-12'>
           <section className='flex flex-col items-center inter-200 bg-black/30 py-4' >
             <h1 className='text-white/50 text-2xl'>Candies Center</h1>
             <span className=' text-white/40 text-xs'>Alacant, Barcelona, Madrid</span>
@@ -24,26 +24,26 @@ function HomePage() {
        </div>
 
 
-      <div className='mb-24 w-full'>
+      <div className='mb-24'>
 
         <section className='flex flex-col mx-12 mb-4 text-center border-y py-3 border-white/20'>
-          <h1 className='ml-16 inter-800 w-full text-xl text-white/60 flex items-center gap-[6px] '>Newest Creation <span className='text-[10px] bg-black/70 p-1 '>$ 6.99</span></h1>
+          <h1 className='ml-14 inter-800 text-xl text-white/60 flex items-center gap-[6px] '>Newest Creation <span className='text-[10px] bg-black/70 p-1 '>$ 6.99</span></h1>
           <span className='text-center text-white/50 inter-300 mt-[-3px] text-[10px]'>cereza con aromas tropicales</span>
         </section>
 
-            <section className='mx-10'>
+
               <div className='flex mb-4'>
 
-                  <section className='flex flex-col text-[8px] text-white/70 gap-1 inter-200 ml-5 mr-4 mt-10 '>
+                <section className='flex flex-col text-[8px] text-white/70 gap-1 inter-200 ml-5 mr-4 mt-10 '>
                     <span className='py-1 px-2 rounded-md bg-[rgba(16,43,133,0.6)] border-black/80 w-fit text-[#6768c0]'>Blueberry</span>
                     <span className='py-1 px-2 rounded-md bg-[rgb(133,16,65,0.6)] border-black/80 w-fit text-[#a86386]'>Strawberry</span>
                     <span className='py-1 px-2 rounded-md bg-[rgb(104,85,22,0.6)] border-black/80 w-fit text-[#9e8f3c]'>Coconut</span>
                     <span className='py-1 px-2 rounded-md bg-[rgba(37,85,37,0.6)] border-black/80 w-fit text-[#63a880]'>Limonade</span>
-                  </section>
-
+                </section>
                 <img src={ChocoTwo} alt="" className='h-40' />
 
               </div>
+
               <div className='flex flex-col items-center gap-1'>
                 <section className='[&>*]:h-4 flex '>
                   <img src={Star} alt="" />
@@ -54,7 +54,7 @@ function HomePage() {
                 </section>
                 <p className='text-white/40 text-[8px] text-center w-40 inter-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aliquam accusantium quaerat.</p>
               </div>
-            </section>
+
 
 
       </div>
