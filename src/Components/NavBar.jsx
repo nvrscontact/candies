3c994b3610bd7fac5c45ad19f1ad6f051f0af18f
nvrsunk <nvrsdark@gmail.com>
2025-07-16 17:19:34 +0200

    g

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,12 +5,14 @@ function NavBar() {
   return (
 
     <>
-    <section className='flex gap-4 inter-200 text-[12px] w-full items-center justify-center mt-6 text-white/60 '>
+    <section className='flex mx-2 inter-200 text-[12px]'>
+      <div className='w-full rounded-b-xl flex gap-4 justify-center pt-5 pb-4 text-white/60 bg-white/10'>
       <Link to='/'>Home</Link>
       <Link to='/aboutme'>About Us</Link>
       <Link to='/aboutme'>Gallery</Link>
       <Link to='/aboutme'>Store</Link>
       <Link to='/aboutme'>Carreers</Link>
+      </div>
     </section>
     </>
   )
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -12,26 +12,29 @@ function HomePage() {
   return (
     <>
     
-    <section className='mt-8 flex flex-col items-center '>
+    <section className='mt-10 flex flex-col items-center '>
        
-      <div className='flex flex-col items-center inter-200 mb-12 border-y w-full py-4 border-white/20 ' >
+      <div className='flex flex-col items-center inter-200 mb-12 ' >
       <h1 className='text-white/50 text-2xl'>Candies Center</h1>
       <span className=' text-white/40 text-xs'>Alacant, Barcelona, Madrid</span>
       </div>
 
 
-      <div className=''>
+      <div className='mb-12 w-full'>
 
-        <section className='flex flex-col mb-4 items-center'>
+        <section className='flex flex-col mx-12 mb-4 items-center border-y py-3 border-white/20'>
           <h1 className='inter-800 text-xl text-center text-white/60'>Newest Creation</h1>
           <span className='text-center text-white/50 inter-300 mt-[-3px] text-[10px] '>cereza con aromas tropicales</span>
         </section>
 
-        <section className='flex flex-col items-center'>
+        <section className=''>
 
-
-          <img src={ChocoTwo} alt="" className='h-52 w-52' />
+          <div className='flex'>
+            <span className='mt-10 flex flex-col inter-200 text-white/40 text-center ml-9 text-sm '><span className='text-white/80 bg-black/40 py-1 px-2 rounded-md'>$ 6.99</span></span>
+            <img src={ChocoTwo} alt="" className='h-52 w-52' />
+          </div>
           <div className='flex flex-col items-center mt-2'>
+            <h2 className='text-white/40 mb-2 text-s '>Available Tastes</h2>
               <section className='flex items-center text-[10px] text-white/70 gap-1 '>
                 <span className='py-1 px-2 rounded-md bg-[rgba(16,43,133,0.6)] border-black/80 w-fit text-[#6768c0] '>Blueberry</span>
                 <span className='py-1 px-2 rounded-md bg-[rgb(133,16,65,0.6)] border-black/80 w-fit text-[#a86386] '>Strawberry</span>
@@ -45,11 +48,23 @@ function HomePage() {
 
       </div>
 
+
+        <div className=''>
+
+        <section className='flex flex-col mb-4 items-center border-y py-3 border-white/20'>
+          <h1 className='inter-800 text-xl text-center text-white/60'>Summer Delight</h1>
+          <span className='text-center text-white/50 inter-300 mt-[-3px] text-[10px] '>cereza con aromas tropicales</span>
+        </section>
+
+    
+
+      </div>
+
       <div>
 
         <section className='flex flex-col items-center gap-1 mb-2 mt-10'>
           <span className='text-[9px] text-white/40 '>Pay with your favorites</span>
-          <div className='flex gap-1 [&>*]:h-5 '>
+          <div className='flex gap-1 [&>*]:h-6 '>
             <img src={ApplePay} alt="" />
             <img src={Mastercard} alt="" />
             <img src={AmericanExpress} alt="" />
