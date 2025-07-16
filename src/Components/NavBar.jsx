23fb6f5c38e33796a19d443bcb519cf8c66a37e8
nvrsunk <nvrsdark@gmail.com>
2025-07-16 17:23:54 +0200

    g

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,8 +5,8 @@ function NavBar() {
   return (
 
     <>
-    <section className='flex mx-2 inter-200 text-[12px]'>
-      <div className='w-full rounded-b-xl flex gap-4 justify-center pt-5 pb-4 text-white/60 bg-white/10'>
+    <section className='flex mt-2 mx-2 inter-200 text-[12px]'>
+      <div className='w-full rounded-xl flex gap-4 justify-center py-4 text-white/60 bg-white/10'>
       <Link to='/'>Home</Link>
       <Link to='/aboutme'>About Us</Link>
       <Link to='/aboutme'>Gallery</Link>
