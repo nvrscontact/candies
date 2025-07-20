053a4ebc1f93c3dc1f4169eadebc058968895558
nvrsunk <nvrsdark@gmail.com>
2025-07-20 22:11:24 +0200

    g

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, {useRef} from 'react'
 import {Link} from 'react-router-dom'
-import HamburgerMenu from '../assets/hamburger_menu.svg'
+import HamburgerMenu from '../assets/hamburger_right.svg'
 
 function NavBar() {
 
@@ -19,9 +19,13 @@ function NavBar() {
 
     <>
 
-    <section className='bg-black/30 '>
+    <section className='bg-black/30 flex justify-between px-6 py-[10px] '>
 
-      <button onClick={toggleMenu} className='py-3 pl-6' > <img src={HamburgerMenu} alt="" className='h-7' />
+      <div className='flex flex-col'>
+        <h1 className='text-white/70 text-[16px] '>Candies Center</h1>
+        <span className='text-[10px] text-white/50 '>Alacant, Barcelona, Madrid</span>
+      </div>
+      <button onClick={toggleMenu} className=''> <img src={HamburgerMenu} alt="" className='h-8' />
         <div ref={menuRef} className='hidden absolute'>
           <ul className=' inter-200 text-[12px] bg-white/10 backdrop-blur-xl flex flex-col gap-[2px] text-start border-[2px] border-[#241603] rounded-xl p-1 ml-2 mt-2'>
               <span className='bg-black/50 py-1 px-2 rounded-t-md'><Link to='/'>About us</Link></span>
@@ -31,6 +35,8 @@ function NavBar() {
           </ul>
         </div>
       </button>
+
+
     </section>
     </>
   )
