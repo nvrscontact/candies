7f4bea96ce71b2147ae51302dadfcce8ff0be9f2
nvrsunk <nvrsdark@gmail.com>
2025-07-20 22:16:08 +0200

    g

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -19,13 +19,13 @@ function NavBar() {
 
     <>
 
-    <section className='bg-black/30 flex justify-between px-6 py-[10px] '>
+    <section className='bg-black/30 border-b border-white/30 flex justify-between py-3 '>
 
-      <div className='flex flex-col'>
-        <h1 className='text-white/70 text-[16px] '>Candies Center</h1>
+      <div className='flex flex-col pl-6'>
+        <h1 className='text-white/70 text-[16px] '>Candies Center<span className='text-[14px] ' >®</span></h1>
         <span className='text-[10px] text-white/50 '>Alacant, Barcelona, Madrid</span>
       </div>
-      <button onClick={toggleMenu} className=''> <img src={HamburgerMenu} alt="" className='h-8' />
+      <button onClick={toggleMenu} className='pr-3'> <img src={HamburgerMenu} alt="" className='h-8' />
         <div ref={menuRef} className='hidden absolute'>
           <ul className=' inter-200 text-[12px] bg-white/10 backdrop-blur-xl flex flex-col gap-[2px] text-start border-[2px] border-[#241603] rounded-xl p-1 ml-2 mt-2'>
               <span className='bg-black/50 py-1 px-2 rounded-t-md'><Link to='/'>About us</Link></span>
